17d699ca599ce0cdead1744a917845a14bafdb70
Gaurav <gauravtak996@gmail.com>
2022-08-14 01:08:45 +0530

    update 3.0 before completion

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ function changeBg(){
     let scrollValue = window.scrollY;
     // console.log(scrollValue);
 
-    if (scrollValue < 500) {
+    if (scrollValue < 490) {
         body.classList.remove('bgColor-1')
     }else{
         body.classList.add('bgColor-1')
     }
 
     
-    if (scrollValue < 1200) {
+    if (scrollValue < 1098) {
         body.classList.remove('bgColor-2')
     }else{
         body.classList.add('bgColor-2')
     }
 
 
-    if (scrollValue < 1880) {
+    if (scrollValue < 1601) {
         body.classList.remove('bgColor-3')
     }else{
         body.classList.add('bgColor-3')
     }
 
-    if (scrollValue < 2552) {
+    if (scrollValue < 2301) {
         body.classList.remove('bgColor-4')
     }else{
         body.classList.add('bgColor-4')
     }
 
 
-    if (scrollValue < 3310) {
+    if (scrollValue < 2901) {
         body.classList.remove('bgColor-5')
     }else{
         body.classList.add('bgColor-5')
