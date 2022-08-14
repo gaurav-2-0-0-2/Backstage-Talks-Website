1d4f6536f72a3e51212139c3ff93c107c3a1390a
Gaurav <gauravtak996@gmail.com>
2022-08-14 09:58:56 +0530

    responsiveness failed 😐

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ function changeBg(){
     }
 
 
-    if (scrollValue < 1601) {
+    if (scrollValue < 1890) {
         body.classList.remove('bgColor-3')
     }else{
         body.classList.add('bgColor-3')
     }
 
-    if (scrollValue < 2301) {
+    if (scrollValue < 2640) {
         body.classList.remove('bgColor-4')
     }else{
         body.classList.add('bgColor-4')
     }
 
 
-    if (scrollValue < 2901) {
+    if (scrollValue < 3401) {
         body.classList.remove('bgColor-5')
     }else{
         body.classList.add('bgColor-5')
