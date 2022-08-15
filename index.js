79212648ee5dd2f281ebb6675c69b293dc525087
Gaurav <gauravtak996@gmail.com>
2022-08-15 16:01:00 +0530

    comments added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function changeBg(){
     let body = document.querySelector('body');
 
-    let scrollValue = window.scrollY;
+    let scrollValue = window.scrollY; // gives scroll value of viewport.
     // console.log(scrollValue);
 
     if (scrollValue < 490) {
@@ -12,27 +12,27 @@ function changeBg(){
     }
 
     
-    if (scrollValue < 1098) {
+    if (scrollValue < 1298) {
         body.classList.remove('bgColor-2')
     }else{
         body.classList.add('bgColor-2')
     }
 
 
-    if (scrollValue < 1650) {
+    if (scrollValue < 1850) {
         body.classList.remove('bgColor-3')
     }else{
         body.classList.add('bgColor-3')
     }
 
-    if (scrollValue < 2240) {
+    if (scrollValue < 2640) {
         body.classList.remove('bgColor-4')
     }else{
         body.classList.add('bgColor-4')
     }
 
 
-    if (scrollValue < 3001) {
+    if (scrollValue < 3301) {
         body.classList.remove('bgColor-5')
     }else{
         body.classList.add('bgColor-5')
