f33e6647c42b3691a7d09eb42e9eb1b0e584b193
Gaurav Tak <gauravtak996@gmail.com>
2023-01-01 12:38:12 +0530

    modified text overflow

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let scrollToCalled = false;
 
-function changeBg(){
+function changeBg() {
     let body = document.querySelector('body');
 
     let scrollValue = window.scrollY; // gives scroll value of viewport.
@@ -7,40 +8,57 @@ function changeBg(){
 
     if (scrollValue < 490) {
         body.classList.remove('bgColor-1')
-    }else{
+    } else {
         body.classList.add('bgColor-1')
     }
 
-    
+
     if (scrollValue < 1298) {
         body.classList.remove('bgColor-2')
-    }else{
+    } else {
         body.classList.add('bgColor-2')
     }
 
 
     if (scrollValue < 1850) {
         body.classList.remove('bgColor-3')
-    }else{
+    } else {
         body.classList.add('bgColor-3')
     }
 
     if (scrollValue < 2640) {
         body.classList.remove('bgColor-4')
-    }else{
+    } else {
         body.classList.add('bgColor-4')
     }
 
-
     if (scrollValue < 3301) {
         body.classList.remove('bgColor-5')
-    }else{
+    } else {
         body.classList.add('bgColor-5')
     }
+    // if (!scrollToCalled) {
+    //     let card = document.querySelector("")
+
+    //     var targetTop = window.scrollY + (card.offsetTop - (window.offsetTop / 2)) + (card.offsetHeight / 2)
+
+
+
+    //     window.scrollTo({
+    //         top: targetTop,
+    //         behavior: 'smooth'
+    //     });
+    //     scrollToCalled = true;
+
+    //     window.removeEventListener('scroll', changeBg);
+    // }
+
+
 
 }
 
 
-window.addEventListener('scroll',changeBg);
+window.addEventListener('scroll', changeBg);
+
 
 
