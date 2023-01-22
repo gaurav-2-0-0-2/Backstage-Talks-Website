b49d99baf44c6802ff5df4815c0318d099787a43
Gaurav Tak <gauravtak996@gmail.com>
2023-01-22 08:39:51 +0530

    buy page added

---
+++ buy.js
@@ -0,0 +1,18 @@
+function init() { 
+ 
+var button = document.getElementById('menu');
+
+button.addEventListener('click', ()=>{
+  console.log("yo");
+});
+ }
+
+
+
+
+// const button = document.querySelector("menu")
+
+// button.addEventListener("click", ()=>{
+//     console.log("yo");
+//     // document.body.nav.classList.add("nav-is-open")
+// })
